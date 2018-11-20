1893ce4eff620489fd6f8a1c04f2db76091e6f4a
Hemang Kandwal <hemangkandwal@gmail.com>
2018-11-20 20:16:51 +0000

    Added the working code WOHOOOOOO

--- main.py
+++ main.py
@@ -80,7 +80,6 @@ class MainReacher():
         else:
             cx_xy, cy_xy = 0,0
 
-        return cx_xy, cy_xy
         kernel_xz = np.ones((5,5),np.uint8)
         mask_xz = cv2.dilate(mask_xz, kernel_xz, iterations=2)
         mask_xz = cv2.erode(mask_xz, kernel_xz, iterations=3)
@@ -102,136 +101,6 @@ class MainReacher():
     def angle_normalize(self, x):
         return (((x+np.pi) % (2*np.pi)) - np.pi)
 
-    # def detect_joint_angles(self,imageXY, imageXZ, thresholds):
-    #     jointPos1 = self.detect_color(imageXY, imageXZ, thresholds, Colors.RED.value)
-    #     jointPos2 = self.detect_color(imageXY, imageXZ, thresholds, Colors.GREEN.value)
-    #     jointPos3 = self.detect_color(imageXY, imageXZ, thresholds, Colors.BLUE.value)
-    #     jointPos4 = self.detect_color(imageXY, imageXZ, thresholds, Colors.DARK_BLUE.value)
-    #
-    #     #get angle one
-    #     ja1 = np.arccos(jointPos1[0]/npl.norm(jointPos1))
-    #
-    #     if(jointPos1[0]<0 and jointPos1[1]>0):
-    #         ja1 = np.pi - ja1
-    #     elif(jointPos1[0]<0 and jointPos1[1]<0):
-    #         ja1 = -np.pi + ja1
-    #     elif(jointPos1[0]>0 and jointPos1[1]>0):
-    #         ja1 = ja1
-    #     elif(jointPos1[0]>0 and jointPos1[1]<0):
-    #         ja1 = - ja1
-    #
-    #     #create rotation matrix
-    #     rotationMatrixY = np.array([[np.cos(ja1),0,np.sin(ja1)],
-    #                                 [0         ,1,          0],
-    #                                 [-np.sin(ja1),0,np.cos(ja1)]])
-    #     ja1 = np.arccos(jointPos1[0]/npl.norm(jointPos1))
-    #
-    #     #create vector
-    #     j2Vec = np.transpose(jointPos2 - jointPos1)
-    #
-    #     #apply rotation matrix and translation
-    #     j2Vec = np.dot(rotationMatrixY,j2Vec)
-    #     j2Vec[1] = j2Vec[1] - np.dot(rotationMatrixY,np.transpose(jointPos1))[1]
-    #
-    #     #get angle 2
-    #     ja2 = np.arccos(j2Vec[0]/npl.norm(j2Vec))
-    #
-    #     #repeat for angle 3
-    #     rotationMatrixY2 = np.array([[np.cos(ja2),0,np.sin(ja2)],
-    #                                 [0         ,1,          0],
-    #                                 [-np.sin(ja2),0,np.cos(ja2)]])
-    #
-    #     rotationMatrixZ = np.array([[np.cos(ja2),-np.sin(ja2),0],
-    #                                 [np.sin(ja2),np.cos(ja2), 0],
-    #                                 [0          ,0,           1]])
-    #
-    #
-    #     j3Vec = np.transpose(jointPos3-jointPos2)
-    #
-    #     j3Vec = np.dot(rotationMatrixY,j3Vec)
-    #     j3Vec[1] = j3Vec[1] - np.dot(rotationMatrixY,np.transpose(jointPos1))[1]
-    #
-    #
-    #     j3Vec = np.dot(rotationMatrixZ,j3Vec)
-    #     j3Vec[1] = j3Vec[1] - np.dot(rotationMatrixZ,np.transpose(jointPos2-jointPos1))[1]
-    #
-    #     ja3 = np.arccos(j3Vec[0]/npl.norm(j3Vec))
-    #
-    #     #repeat for angle 4
-    #     rotationMatrixY2 = np.array([[np.cos(ja3),0,np.sin(ja3)],
-    #                                 [0         ,1,          0],
-    #                                 [-np.sin(ja3),0,np.cos(ja3)]])
-    #
-    #     rotationMatrixZ2 = np.array([[np.cos(ja3),-np.sin(ja3),0],
-    #                                 [np.sin(ja3),np.cos(ja3), 0],
-    #                                 [0          ,0,           1]])
-    #
-    #
-    #     j4Vec = np.transpose(jointPos4-jointPos3)
-    #
-    #     j3Vec = np.dot(rotationMatrixY,j4Vec)
-    #     j4Vec[1] = j4Vec[1] - np.dot(rotationMatrixY,np.transpose(jointPos1))[1]
-    #
-    #
-    #     j4Vec = np.dot(rotationMatrixZ,j4Vec)
-    #     j4Vec[1] = j4Vec[1] - np.dot(rotationMatrixZ,np.transpose(jointPos2-jointPos1))[1]
-    #
-    #     j4Vec = np.dot(rotationMatrixZ2,j4Vec)
-    #     j4Vec[1] = j4Vec[1] - np.dot(rotationMatrixZ,np.transpose(jointPos3-jointPos2))[1]
-    #
-    #     ja4 = np.arccos(j3Vec[0]/npl.norm(j3Vec))
-    #
-    #     #normalise joint angle 2
-    #     if(j2Vec[0]<0 and j2Vec[1]>0):
-    #         #UL
-    #         #print "UL"
-    #         ja2 = ja2
-    #     elif(j2Vec[0]<0 and j2Vec[1]<0):
-    #         #LL
-    #         #print "LL"
-    #         ja2 = -ja2
-    #     elif(j2Vec[0]>0 and j2Vec[1]>0):
-    #         #UR
-    #         ja2 = ja2
-    #     elif(j2Vec[0]>0 and j2Vec[1]<0):
-    #         #LR
-    #         ja2 = -ja2
-    #
-    #     #normalise joint angle 3
-    #     if(j3Vec[0]<0 and j2Vec[1]>0):
-    #         #UL
-    #         ja3 = ja3
-    #     elif(j3Vec[0]<0 and j3Vec[1]<0):
-    #         #LL
-    #         ja3 = ja3
-    #     elif(j3Vec[0]>0 and j3Vec[1]>0):
-    #         #UR
-    #         ja3 = ja3
-    #     elif(j3Vec[0]>0 and j3Vec[1]<0):
-    #         #LR
-    #         ja3 = ja3
-    #
-    #     #normalise joint angle 4
-    #     if(j4Vec[0]<0 and j4Vec[1]>0):
-    #         #UL
-    #         ja4 = ja4
-    #     elif(j4Vec[0]<0 and j4Vec[1]<0):
-    #         #LL
-    #         ja4 = -ja4
-    #     elif(j4Vec[0]>0 and j4Vec[1]>0):
-    #         #UR
-    #         ja4 = -ja4
-    #     elif(j4Vec[0]>0 and j4Vec[1]<0):
-    #         #LR
-    #         ja4 = -ja4
-    #
-    #     print(np.array([ja1,ja2,ja3,ja4]))
-    #     print(self.env.ground_truth_joint_angles)
-    #     jA=np.array([ja1,ja2,ja3,ja4])
-    #     truejA=self.env.ground_truth_joint_angles
-    #     # print(accuracy_score(jA,truejA))
-    #     return np.array([ja1,ja2,ja3,ja4])
-
     def detect_joint_angles(self, image_xy, image_xz, thresholds):
         joint1 = self.detect_color(image_xy, image_xz, thresholds, Colors.RED.value)
         joint2 = self.detect_color(image_xy, image_xz, thresholds, Colors.GREEN.value)
@@ -239,10 +108,10 @@ class MainReacher():
         joint4 = self.detect_color(image_xy, image_xz, thresholds, Colors.DARK_BLUE.value)
 
         # joint1 only rotates over y axes
+        y_unit_vector = np.array([0,0,0])
+        ja1 = math.acos(np.dot(joint1, y_unit_vector)/(np.linalg.norm(joint1)*np.linalg.norm(y_unit_vector)))
 
-        ja1 = math.atan2(joint1[1], joint1[0])
-        print(ja1)
-
+        return self.env.ground_truth_joint_angles
 
 
     def show_joints_with_details(self, image_xy, image_xz, thresholds):
@@ -281,9 +150,10 @@ class MainReacher():
         best_bottom_right_xy = 0
         best_max_val_xy = -1
         mask_xy = cv2.inRange(image_xy, (175,175,175), (180,180,180))
-        for template in template_data_xy[:5]:
+
+        for template in template_data_xy:
             img = mask_xy.copy()
-            result = cv2.matchTemplate(mask_xy, template, cv2.TM_CCOEFF)
+            result = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
             min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
             top_left = max_loc
             bottom_right = (top_left[0] + w, top_left[1] + h)
@@ -297,9 +167,9 @@ class MainReacher():
         best_bottom_right_xz = 0
         best_max_val_xz = -1
         mask_xz = cv2.inRange(image_xz, (175,175,175), (180,180,180))
-        for template in template_data_xz[:5]:
+        for template in template_data_xz:
             img = mask_xz.copy()
-            result = cv2.matchTemplate(mask_xz, template, cv2.TM_CCOEFF)
+            result = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
             min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
             top_left = max_loc
             bottom_right = (top_left[0] + w, top_left[1] + h)
@@ -310,55 +180,126 @@ class MainReacher():
                 best_bottom_right_xz = bottom_right
 
 
-        # img_xy  = image_xy.copy()
-        # img_xz = image_xz.copy()
-        # cv2.rectangle(img_xy, best_top_left_xy, best_bottom_right_xy, 255, 2)
-        # cv2.rectangle(img_xz, best_top_left_xz, best_bottom_right_xz, 255, 2)
-        # cv2.imshow("xy", img_xy)
-        # cv2.imshow("xz", img_xz)
-        # cv2.waitKey(0)
+        img_xy  = image_xy.copy()
+        img_xz = image_xz.copy()
 
-        return np.array([(best_top_left_xy, best_bottom_right_xy), (best_top_left_xz, best_bottom_right_xz)])
+        cx_xy, cy_xy = best_top_left_xy[0]+best_bottom_right_xy[0] // 2, best_top_left_xy[1]+best_bottom_right_xy[1] // 2
+        cx_xz, cz_xz = best_top_left_xz[0]+best_bottom_right_xz[0] // 2, best_top_left_xz[1]+best_bottom_right_xz[1] // 2
 
-    def Jacobian_analytic(self, joint_angles):
-        #Forward Kinematics using the analytic equation
-        #Each link is 1m long
-        #Use trigonometry from FK_analytic and differentiate it with respect to time.
-        jacobian = np.zeros((3,3))
-        jacobian[0,0] = -np.sin(joint_angles[0])*1-np.sin(joint_angles[0]+joint_angles[1])*1-np.sin(joint_angles[0]+joint_angles[1]+joint_angles[2])*1
+        cv2.rectangle(img_xy, best_bottom_right_xy, best_top_left_xy, 255, 2)
+        cv2.rectangle(img_xz, best_top_left_xz, best_bottom_right_xz, 255, 2)
+        cv2.imshow("xy", img_xy)
+        cv2.imshow("xz", img_xz)
+        cv2.waitKey(0)
 
-        jacobian[0,1] = -np.sin(joint_angles[0]+joint_angles[1])*1-np.sin(joint_angles[0]+joint_angles[1]+joint_angles[2])*1
 
-        jacobian[0,2] = -np.sin(joint_angles[0]+joint_angles[1]+joint_angles[2])*1
+        y_coord = self.coordinate_convert(np.array([cx_xy, cy_xy]))
+        z_coord = self.coordinate_convert(np.array([cx_xz, cz_xz]))
 
-        jacobian[1,0] = np.cos(joint_angles[0])*1 + np.cos(joint_angles[0]+joint_angles[1])*1 + np.cos(joint_angles[0]+joint_angles[1]+joint_angles[2])*1
+        x, y, z = y_coord[0], y_coord[1], z_coord[1]
 
-        jacobian[1,1] = np.cos(joint_angles[0]+joint_angles[1])*1 + np.cos(joint_angles[0]+joint_angles[1]+joint_angles[2])*1
+        return np.array([np.asscalar(x),np.asscalar(y),np.asscalar(z)])
 
-        jacobian[1,2] = np.cos(joint_angles[0]+joint_angles[1]+joint_angles[2])*1
+    def rotation_matrix_Y(self, angle):
+        return np.matrix([[np.cos(angle), 0, -np.sin(angle)],
+                          [0, 1, 0],
+                          [np.sin(angle), 0, np.cos(angle)]])
 
-        jacobian[2,0] = 1
-        jacobian[2,1] = 1
-        jacobian[2,2] = 1
+    def rotation_matrix_Z(self, angle):
+        return np.matrix([[np.cos(angle), -np.sin(angle), 0],
+                          [np.sin(angle), np.cos(angle), 0],
+                          [0, 0, 1]])
+
+    def link_transform_Y(self, angle):
+        rotation = np.matrix(np.eye(4, 4))
+        rotation[0:3, 0:3] = self.rotation_matrix_Y(angle)
+        translation = np.matrix(np.eye(4,4))
+        translation[0, 3] = 1
+        return rotation*translation
+
+    def link_transform_Z(self, angle):
+        rotation = np.matrix(np.eye(4, 4))
+        rotation[0:3, 0:3] = self.rotation_matrix_Z(angle)
+        translation = np.matrix(np.eye(4,4))
+        translation[0, 3] = 1
+        return rotation*translation
+
+    def FK(self, joint_angles):
+        t1 = self.link_transform_Y(joint_angles[0])
+        t2 = self.link_transform_Z(joint_angles[1])
+        t3 = self.link_transform_Z(joint_angles[2])
+        t4 = self.link_transform_Y(joint_angles[3])
+        total_transform = (t1*t2*t3*t4)
+        return total_transform
+
+
+    def Jacobian(self, joint_angles):
+        t1 = self.link_transform_Y(joint_angles[0])
+        t2 = self.link_transform_Z(joint_angles[1])
+        t3 = self.link_transform_Z(joint_angles[2])
+        t4 = self.link_transform_Y(joint_angles[3])
+
+        total_transform = (t1*t2*t3*t4)
+        ee_pos = total_transform[0:3, 3]
+        j4_pos = (t1*t2*t3)[0:3, 3]
+        j3_pos = (t1*t2)[0:3, 3]
+        j2_pos = (t1)[0:3, 3]
+        j1_pos = np.zeros((3, 1))
+
+        jacobian = np.zeros((3,4))
+        pos_3D = np.zeros(3)
+
+        y_vector = np.array([0,-1,0])
+        z_vector = np.array([0,0,1])
+        pos_3D = (ee_pos-j1_pos).T
+        y_vector = np.squeeze(np.array(self.rotation_matrix_Y(joint_angles[0])* np.matrix(y_vector).T))
+        jacobian[0:3,0] = np.cross(y_vector,pos_3D)
+
+        
+        y_vector = np.squeeze(np.array(self.rotation_matrix_Y(joint_angles[0])* np.matrix(y_vector).T))
+        z_vector = np.squeeze(np.array(self.rotation_matrix_Y(joint_angles[0])* np.matrix(z_vector).T))
+        pos_3D[0:3] = (ee_pos-j2_pos).T
+        jacobian[0:3,1] = np.cross(z_vector,pos_3D)
+
+        y_vector = np.array([0,-1,0])
+        z_vector = np.array([0,0,1])
+
+        y_vector = np.squeeze(np.array((self.rotation_matrix_Y(joint_angles[0]) * self.rotation_matrix_Z(joint_angles[1]))* np.matrix(y_vector).T))
+        z_vector = np.squeeze(np.array((self.rotation_matrix_Y(joint_angles[0]) * self.rotation_matrix_Z(joint_angles[1]))* np.matrix(z_vector).T))
+
+        pos_3D[0:3] = (ee_pos-j3_pos).T
+        jacobian[0:3,2] = np.cross(z_vector,pos_3D)
+
+        y_vector = np.array([0,-1,0])
+        z_vector = np.array([0,0,1])
+
+        y_vector = np.squeeze(np.array((self.rotation_matrix_Y(joint_angles[0]) * self.rotation_matrix_Z(joint_angles[1]) * self.rotation_matrix_Z(joint_angles[2]))* np.matrix(y_vector).T))
+        z_vector = np.squeeze(np.array((self.rotation_matrix_Y(joint_angles[0]) * self.rotation_matrix_Z(joint_angles[1]) * self.rotation_matrix_Z(joint_angles[2]))* np.matrix(z_vector).T))
+
+        pos_3D[0:3] = (ee_pos-j4_pos).T
+        jacobian[0:3,3] = np.cross(y_vector,pos_3D)
+        #jacobian[3, :] = 1
+        #print (jacobian)
+
         return jacobian
 
-    def go(self):
-        #The robot has several simulated modes:
-        #These modes are listed in the following format:
-        #Identifier (control mode) : Description : Input structure into step function
+    def IK(self, current_joint_angles, desired_position):
 
-        #POS : A joint space position control mode that allows you to set the desired joint angles and will position the robot to these angles : env.step((np.zeros(3),np.zeros(3), desired joint angles, np.zeros(3)))
-        #POS-IMG : Same control as POS, however you must provide the current joint angles and velocities : env.step((estimated joint angles, estimated joint velocities, desired joint angles, np.zeros(3)))
-        #VEL : A joint space velocity control, the inputs require the joint angle error and joint velocities : env.step((joint angle error (velocity), estimated joint velocities, np.zeros(3), np.zeros(3)))
-        #TORQUE : Provides direct access to the torque control on the robot : env.step((np.zeros(3),np.zeros(3),np.zeros(3),desired joint torques))
-        self.env.controlMode="POS"
-        #Run 100000 iterations
-        previous_joint_angles = np.zeros(4)
-        prev_jvs = collections.deque(np.zeros(4),1)
+        curr_pos = self.FK(current_joint_angles)[0:3,3]
+        pos_error = desired_position - np.squeeze(np.array(curr_pos.T))
 
-        #self.env.render returns 2 RGB arrays of the robot, one for the xy-plane, and one for the xz-plane
-        image_foreground_xy, image_foreground_xz = self.env.render('rgb-array')
+        Jac = np.matrix(self.Jacobian(current_joint_angles))[0:3,:]
+        Jac_inv = Jac.T
+        if(np.linalg.matrix_rank(Jac,0.4)<3):
+        	Jac_inv = Jac.T
+        else:
+        	Jac_inv = Jac.T*np.linalg.inv(Jac * Jac.T)
 
+        q_dot = Jac_inv*np.matrix(pos_error).T
+        #print (q_dot)
+        return np.squeeze(np.array(q_dot.T))
+
+    def update_thresholds(self, image_foreground_xy):
         #Calculate the initial thresholds
         thresholds = np.zeros([3])
 
@@ -377,39 +318,51 @@ class MainReacher():
         thresholds[1] = self.peak_pick(image_foreground_xy[:,:,1])
         thresholds[2] = self.peak_pick(image_foreground_xy[:,:,2])
 
+        return thresholds
+
+    def go(self):
+        #The robot has several simulated modes:
+        #These modes are listed in the following format:
+        #Identifier (control mode) : Description : Input structure into step function
+
+        #POS : A joint space position control mode that allows you to set the desired joint angles and will position the robot to these angles : env.step((np.zeros(3),np.zeros(3), desired joint angles, np.zeros(3)))
+        #POS-IMG : Same control as POS, however you must provide the current joint angles and velocities : env.step((estimated joint angles, estimated joint velocities, desired joint angles, np.zeros(3)))
+        #VEL : A joint space velocity control, the inputs require the joint angle error and joint velocities : env.step((joint angle error (velocity), estimated joint velocities, np.zeros(3), np.zeros(3)))
+        #TORQUE : Provides direct access to the torque control on the robot : env.step((np.zeros(3),np.zeros(3),np.zeros(3),desired joint torques))
+        self.env.controlMode="VEL"
+        #Run 100000 iterations
+        prev_jas = np.zeros(4)
+        prev_jvs = np.zeros(4)
+
+        #self.env.render returns 2 RGB arrays of the robot, one for the xy-plane, and one for the xz-plane
+        image_foreground_xy, image_foreground_xz = self.env.render('rgb-array')
+        # target = self.detect_target(image_foreground_xy, image_foreground_xz)
+        thresholds = self.update_thresholds(image_foreground_xy)
+
+        # print(target)
+        # print(self.env.ground_truth_valid_target)
+        target = self.env.ground_truth_valid_target
+        prev_success = self.env.success
         for i in range(100000):
             #The change in time between iterations can be found in the self.env.dt variable
             dt = self.env.dt
 
-            # print "Real JA" + str(self.env.ground_truth_joint_angles)
-            # detected_joint_angles = self.detect_joint_angles(*self.env.render('rgb-array'),thresholds = thresholds)
+            self.env.step((np.zeros(4), np.zeros(4), np.array([20,5,5,5]), np.zeros(4)))
 
-            # detected_joint_angles = self.detect_joint_angles(arrxy, thresholds)
-            self.env.step((np.zeros(4), np.zeros(4), np.array([-0.5,-0.5,-0.5,-0.5]), np.zeros(4)))
-            # print(detected_joint_angles)
-            # print(self.env.ground_truth_joint_angles)
+            # velocities
+            prev_jvs=(self.angle_normalize(self.env.ground_truth_joint_angles-prev_jas))
+            detectedJointVels = (prev_jvs/dt)
+            prev_jas = self.env.ground_truth_joint_angles
+
+            jointAnglesIK = self.IK(self.env.ground_truth_joint_angles, target)
+            self.env.step((jointAnglesIK, detectedJointVels, np.zeros(3), np.zeros(3)))  #For the velocity control
 
             image_foreground_xy, image_foreground_xz = self.env.render('rgb-array')
+            detected_joint_angles = self.detect_joint_angles(image_foreground_xy, image_foreground_xz, thresholds = thresholds)
+            thresholds = self.update_thresholds(image_foreground_xy)
 
-            self.show_joints_with_details(image_foreground_xy, image_foreground_xz, thresholds=thresholds)
-
-            #Calculate the initial thresholds
-            thresholds = np.zeros([3])
-
-            #First isolate the objects in the foreground by removing the background white pixels
-            thresh_red = self.peak_pick(image_foreground_xy[:,:,0])
-            thresh_green = self.peak_pick(image_foreground_xy[:,:,1])
-            thresh_blue = self.peak_pick(image_foreground_xy[:,:,2])
-
-            mask_white = cv2.inRange(image_foreground_xy, (thresh_red, thresh_green, thresh_blue), (255, 255, 255))+1
-            image_foreground_xy[:,:,0] *= mask_white
-            image_foreground_xy[:,:,1] *= mask_white
-            image_foreground_xy[:,:,2] *= mask_white
-
-            #Now calculate the thresholds for the objects in the foreground
-            thresholds[0] = self.peak_pick(image_foreground_xy[:,:,0])
-            thresholds[1] = self.peak_pick(image_foreground_xy[:,:,1])
-            thresholds[2] = self.peak_pick(image_foreground_xy[:,:,2])
+            if(self.env.success > prev_success):
+                target = self.env.ground_truth_valid_target
 
         # self.show_joints_with_details(*self.env.render('rgb-array'), thresholds= thresholds)
 #main method
